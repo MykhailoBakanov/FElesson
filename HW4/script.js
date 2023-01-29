6d4d657686abad6b34a72c9af2e232087d7da644
MykhailoBakanov <117221737+MykhailoBakanov@users.noreply.github.com>
2023-01-29 17:02:29 +0100

    Add files via upload

---
+++ HW4/script.js
@@ -0,0 +1,87 @@
+let a = 60; //sek
+let b = 60; //min
+let c = 24; //hour 
+console.log( a*b , "секунд в часе");
+console.log(a*b*c , ' секунд в сутках');
+//-------------------------
+
+let num = 3 ;
+alert(num)
+//-----------------------
+
+let a1 =10;
+let b1 =2;
+console.log(a1+b1, 'сумма чисел');
+console.log(a1-b1, 'разность чисел');
+console.log(a1*b1, 'произведение чисел');
+console.log(a1%b1, 'частое чисел');
+//--------------------------------
+
+let c1 = 15;
+let d = 2;
+let result = c1 + d;
+console.log(result)
+//-------------------
+
+let a2 = 10;
+let b2 = 2;
+let c2 = 5;
+console.log(a2+b2+c2)
+//----------------------------
+
+let a3 = 17;
+let b3 = 10;
+let c3 = a3-b3;
+let d2 = 7;
+let result1 = c3+d2;
+console.log(result);
+//-------------------------------
+
+let str = 'Привет , Мир!';
+console.log(str)
+//------------------------------- 
+
+let str1 = 'Привет';
+let str2 = ' , Мир!';
+console.log(str1+str2);
+//-------------------------------
+
+let name = 'Mykhailo'
+console.log('Привет , ' + name + "!");
+//-------------------------------
+
+let age = 21;
+console.log("Мне "+ age + " год");
+//-------------------------------
+
+// let a4 = 1;
+//let a4 = 0;
+let a4 =-3; 
+if(a4=0){
+ console.log("true")
+} else {
+ console.log("false")
+}
+
+//-------------------------------
+
+// let a5 = 1;
+//let a5 = 0;
+let a5 =-3; 
+if(a5>0){
+    console.log("true")
+   } else {
+    console.log("false")
+   }
+
+//-------------------------------
+
+// let a6 = 1;
+//let a6 = 0;
+let a6 =-3; 
+if(a6<0){
+    console.log("true")
+   } else {
+    console.log("false")
+   }
+   
