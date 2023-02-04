5f0c952e21942be2d6db26189317044030bd09eb
MykhailoBakanov <117221737+MykhailoBakanov@users.noreply.github.com>
2023-02-04 21:40:54 +0100

    Add files via upload

---
+++ HW5/script.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 3, 4, 5];
+for ( let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+
+for ( let i = 0; i < 100; i++){
+    console.log([i])
+}
+
+for ( let i = 11; i < 33; i++){
+    console.log([i])
+}
+
+let result = 0;
+let array = [1, 2, 3, 4, 5];
+for( let i = 0; i <= array.length; i++){
+    result += i;
+}
+console.log(result)
